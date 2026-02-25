fa2a400119258d034df52b5ab9b39c212a2545a2
Meganimys <meganimysblack@gmail.com>
2026-02-25 04:33:35 +0200

    s

--- src/components/client/NavMenu.tsx
+++ src/components/client/NavMenu.tsx
@@ -218,7 +218,7 @@ const openRegAndCloseLogin = () => {
           </div>
           <div className="">
           <ModalUserMenu isVisible={isUserModalVisible}></ModalUserMenu>
-          {isUserModalVisible && <div className="absolute top-2 right-2 w-5 h-5 z-10 flex justify-center items-center align-middle">X</div>}
+          {isUserModalVisible && <div className="absolute bottom-[-2] right-2 w-5 h-5 z-10 flex justify-center items-center align-middle">X</div>}
           </div>
           </SignedIn>
         </div>
